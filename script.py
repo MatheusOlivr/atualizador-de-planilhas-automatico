05c8f41ed13a96900d38cd0b01a944f0852ec117
matheusolivr <contato.matheusoliv@gmail.com>
2023-06-06 09:06:17 -0300

    Cria o arquivo de configuração do script e orgnaiza melhor o código Foi criado o arquivo onde ficará todos os diretórios de arquivos que precisam ser atualizadoos e o código foi melhor organizado

--- script.py
+++ script.py
@@ -1,12 +1,18 @@
 import time
-import win32com.client as app
-print('iniciar')
-file = app.Dispatch("Excel.Application")
-file.visible = 0
-dirfile = r"C:\Users\Matheus\Downloads\teste.xlsx"
-workbook = file.Workbooks.open(dirfile)
-workbook.RefreshAll()
-time.sleep(10)
-workbook.Save()
-file.Quit()
-exit
+import win32com.client
+import pandas as pd 
+def atualizar_planilha(file):
+    print('iniciar')
+    app = win32com.client.Dispatch("Excel.Application")
+    app.visible = 0
+    workbook = app.Workbooks.open(file)
+    workbook.RefreshAll()
+    time.sleep(10)
+    workbook.Save()
+    app.Quit()
+    exit
+dir = r"C:\Users\Matheus\OneDrive\Nuvem\AMBIENTE_DE_DESENVOLVIMENTO\PYTHON\atualizador_de_dados_externos\config.csv"
+lfile = pd.read_csv(dir,header=None)
+for i in lfile.values:
+    for v in i:
+        atualizar_planilha(lfile)
